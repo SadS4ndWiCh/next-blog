27659fa8a4f197a719bc4f445eab4131078e06ea
SadS4ndWiCh <caiocamargo007@gmail.com>
2021-03-06 22:10:26 -0300

    add: home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,50 @@
-import styles from '../styles/pages/Home.module.css'
+import DefaultLayout from '@layouts/default';
+import Link from '@components/Link';
 
-export default function Home() {
+import { getConfig, getAllPosts } from '@api';
+import { GetStaticProps } from 'next';
+
+import styles from '../styles/pages/Home.module.css';
+
+interface Post {
+  slug: string;
+  title: string;
+}
+
+interface HomePageProps {
+  posts: Post[];
+  title: string;
+  description: string;
+}
+
+export default function Home({ posts, title, description }: HomePageProps) {
   return (
     <div className={styles.container}>
-      <h1>Blog</h1>
+      <DefaultLayout title={title} description={description}>
+        <p>List of posts</p>
+        <ul>
+          { posts.map((post, i) => (
+            <li key={i}>
+              <Link to={`/posts/${post.slug}`}>
+                {post.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </DefaultLayout>
     </div>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const config = await getConfig();
+  const allPosts = await getAllPosts();
+
+  return {
+    props: {
+      posts: allPosts,
+      title: config.title,
+      description: config.description,
+    }
+  }
 }
