6fd73c559c63965a7347b8b7dff6055178a697a6
SadS4ndWiCh <caiocamargo007@gmail.com>
2021-03-07 22:41:24 -0300

    add: new footer content

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,20 @@
+import styles from '@styles/components/Footer.module.css';
+import Link from './Link';
+
 export default function Footer() {
     return (
-        <footer>
-            <p>&copy;2020 | SadS4ndWiCh</p>
+        <footer className={styles.footerContainer}>
+          <div>
+            <img src="/full-logo.svg" alt="Minimalista Logo"/>
+
+            <ul>
+              <li>
+                <Link to='/'>Home</Link>
+              </li>
+            </ul>
+          </div>
+     
+          <p>&copy; 2021 | <strong>Blog Minimalist</strong></p>
         </footer>
     )
 }
