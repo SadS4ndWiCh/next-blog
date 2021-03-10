c1cba938af2007e51eaf71e9330f2b7228f07f56
SadS4ndWiCh <caiocamargo007@gmail.com>
2021-03-09 22:10:31 -0300

    add: importing posts from prismic

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,46 +1,62 @@
+import Prismic from '@prismicio/client';
+import { Document } from '@prismicio/client/types/documents';
+import { RichText } from 'prismic-reactjs';
+import { Client } from '@utils/prismic-configuration';
+
 import PostLayout from '@layouts/post';
 import { getPostBySlug, getPostsSlugs } from '@lib/index';
 import { GetStaticPaths, GetStaticProps } from 'next';
 
 import styles from '../../styles/pages/Post.module.css';
+import formatDate from '@utils/formatDate';
 
 interface PostPageProps {
-  title?: string;
-  date?: string;
-  thumbnail?: string;
-  content?: string;
-  excerpt?: string;
-  slug?: string;
+  post: Document;
 }
 
-export default function Post({ title, content, thumbnail }: PostPageProps) {
+export default function Post({ post }: PostPageProps) {
+  console.log(post.data.content);
+
   return (
     <div className={`${styles.postContainer} container`}>
       <PostLayout
-        title={title}
-        content={content}
-        thumbnail={thumbnail}
+        title={RichText.asText(post.data.title)}
+        content={post.data.content}
+        thumbnail={post.data.thumbnail.url}
       />
     </div>
   )
 }
 
-export const getStaticProps: GetStaticProps = async (ctx) => {
-  const slug = ctx.params.slug as string;
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const slug = params.slug as string;
+  
+  const post = await Client.getByUID('article', slug, {
+    lang: 'pt-br',
+  });
+
+  post.data.formattedDate = formatDate(post.data.published_at);
   
   return {
-    props: getPostBySlug(slug, ['title', 'content', 'thumbnail']),
+    props: {
+      post
+    }
   }
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  let paths = getPostsSlugs();
-  paths = paths.map(post => ({
-    params: { slug: post.replace(/\.md$/, '') }
-  })) as any;
+  const posts = await Client.query(
+    Prismic.Predicates.at('document.type', 'article'),
+  );
+
+  const allBlogPosts = [];
+
+  posts.results.map(post => {
+    allBlogPosts.push({ params: { slug: post.uid } });
+  });
 
   return {
-    paths,
+    paths: allBlogPosts,
     fallback: false,
   }
 }
